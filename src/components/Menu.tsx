462a4d6583d9bac6f55cd6800c0f9ac0af586368
the-ict <dvltinv@gmail.com>
2026-05-09 18:26:34 +0500

    add: position props added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,8 +3,19 @@ import { useState } from "react";
 
 type ActiveTabs = "home" | "services" | "process" | "projects";
 
+const activeTabs = {
+    "home": "Bosh sahifa",
+    "services": "Xizmatlar",
+    "process": "Jarayon",
+    "projects": "Loyihalar",
+};
+
 export default function Menu() {
     const [isActive, setIsActive] = useState<ActiveTabs>("home");
+    const [position, setPosition] = useState({
+        positionLeft: 0,
+        width: 0,
+    })
 
     const handleTabChange = (tab: ActiveTabs) => {
         setIsActive(tab);
@@ -20,19 +31,20 @@ export default function Menu() {
     return (
         <div className="flex items-center my-3 fixed bottom-0 h-[80px] w-screen left-0 right-0 justify-center text-white z-[100]">
             <div className="max-w-[50%] max-md:max-w-full">
-                <ul className="w-full py-3 px-2 transition-all rounded-[35px] bg-gray-200 cursor-pointer flex items-center gap-3 text-xs font-bold">
-                    <li className={getTabClass("home")} onClick={() => handleTabChange("home")}>
-                        <span>Bosh sahifa</span>
-                    </li>
-                    <li className={getTabClass("services")} onClick={() => handleTabChange("services")}>
-                        <span>Xizmatlar</span>
-                    </li>
-                    <li className={getTabClass("process")} onClick={() => handleTabChange("process")}>
-                        <span>Jarayon</span>
-                    </li>
-                    <li className={getTabClass("projects")} onClick={() => handleTabChange("projects")}>
-                        <span>Loyihalar</span>
-                    </li>
+                <ul className="w-full py-3 px-2 transition-all rounded-[35px] bg-gray-200 cursor-pointer flex items-center gap-3 text-xs font-bold relative">
+                    <Cursor position={position} />
+                    {
+                        Object.keys(activeTabs).map((tab, index) => (
+                            <TabItems
+                                key={index}
+                                tab={tab as ActiveTabs}
+                                setPosition={setPosition}
+                                position={position}
+                                getTabClass={getTabClass}
+                                setIsActive={setIsActive}
+                            />
+                        ))
+                    }
                 </ul>
             </div>
         </div>
@@ -45,11 +57,13 @@ type CursorProps = {
     width: number;
 }
 
-const Cursor = (props: CursorProps) => {
+const Cursor = ({ position }: {
+    position: CursorProps
+}) => {
     useGSAP(() => {
         gsap.to(".cursor", {
-            left: props.positionLeft,
-            width: props.width,
+            left: position.positionLeft,
+            width: position.width,
             duration: 0.3,
             ease: "power3.out"
         })
@@ -57,5 +71,23 @@ const Cursor = (props: CursorProps) => {
     return (
         <div className="bg-[#333]/30 cursor-pointer absolute">
         </div>
+    )
+}
+
+type TabItemsProps = {
+    tab: ActiveTabs;
+    setPosition: React.Dispatch<React.SetStateAction<CursorProps>>;
+    position: CursorProps;
+    getTabClass: (tab: ActiveTabs) => string;
+    setIsActive: React.Dispatch<React.SetStateAction<ActiveTabs>>
+}
+const TabItems = ({ tab, setPosition, position, getTabClass, setIsActive }: TabItemsProps) => {
+    const handleTabChange = (tab: ActiveTabs) => {
+        setIsActive(tab);
+    };
+    return (
+        <li className={getTabClass("home")} onClick={() => handleTabChange(tab)}>
+            <span>{activeTabs[tab]}</span>
+        </li>
     )
 }
